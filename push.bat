36110cd9681fcbbb50f7b73f5b9ed9dc24bd29d0
tongjinlv <jinlv.tong@tinno.com>
2025-12-04 15:56:26 +0800

    ok

---
+++ push.bat
@@ -0,0 +1,15 @@
+@echo off
+chdir /d "%~dp0"
+git config --global core.quotepath true
+git status
+set /p commit_msg="please input commit message:"
+if "%commit_msg%"=="" (
+    for /f "tokens=1-3 delims=/" %%a in ('date /t') do set date=%%a%%b%%c
+    for /f "tokens=1-3 delims=:" %%a in ('time /t') do set time=%%a%%b%%c
+    set commit_msg="auto push %date% %time%"
+)
+git add --all
+git add .
+git commit -m "%commit_msg%"
+git push
+pause
